06fa2ac85d3525e3281bcb391630de503338c2ee
AshutoshPC <ashutoshsaxena06@gmail.com>
2017-06-19 20:48:36 +0530

    Added - Lucille restaurant

--- src/test/java/com/edge/CheneyOnline/TestCheneyOnline.java
+++ src/test/java/com/edge/CheneyOnline/TestCheneyOnline.java
@@ -257,17 +257,17 @@ public class TestCheneyOnline extends CommonCheney {
 	// Restaurant");
 	// }
 
-//	@Test(priority = 17)
-//	public void Lucilles_Cheney() throws InterruptedException {
-//
-//		LoginCheney(driver, "60021442CBI", "Lbbq2144");
-//		// check the flow to exports
-//		// StepsToExportCheney(driver);
-//		// rename downloadeds
-//		// String CurrentPath = RandomAction.setdownloadDir();
-//		// File GFS_OG = RandomAction.getLatestFilefromDir(CurrentPath);
-//		SendMailSSL.sendMailAction("Cheney Brothers Inc, Offline", "Lucille's BBQ");
-//	}
+	@Test(priority = 17)
+	public void Lucilles_Cheney() throws InterruptedException {
+
+		LoginCheney(driver, "60021442CBI", "Lbbq2144");
+		// check the flow to exports
+		// StepsToExportCheney(driver);
+		// rename downloadeds
+		// String CurrentPath = RandomAction.setdownloadDir();
+		// File GFS_OG = RandomAction.getLatestFilefromDir(CurrentPath);
+		SendMailSSL.sendMailAction("Cheney Brothers Inc, Offline", "Lucille's BBQ");
+	}
 
 	@Test(priority = 18)
 	public void SamHudsonBeachBar_Cheney() throws InterruptedException {
